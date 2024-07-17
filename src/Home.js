7955138bd4688133f44e8434fabd1311c219f041
zoubaidachatti <71558413+zoubaidachatti@users.noreply.github.com>
2024-07-17 16:13:49 +0100

    use effect / fetch data json

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
+  const [isPanding, setIsPanding] = useState(true);
+  const [error, setError] = useState(null);
+  const [name, setName] = useState('mario');
+  const handleDelete = (id) => {
+  const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() =>{
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogss')
+        .then(res =>
+        {
+          if (!res.ok){
+            throw Error('could not fetch the data for that ressource');
+          }
+          return res.json();
+        }
+        )
+        .then(data => {
+
+        //console.log(data);
+        setBlogs(data);
+        setIsPanding(false);
+        })
+      .
+      catch((err)=>{
+        setIsPanding(false);
+       setError(err.message);
+
+      })
+      
+    }, 1000);
+  },[])
 
   return (
     <div className="home">
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
+      {error && <div>{error }</div>}
+      {isPanding && <div>loading ...</div>}
+      { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> }
+ 
+     
     </div>
   );
 }
- 
 export default Home;
